45c4f4b13e0a13d833af6174cb5c296b928bf055
oooooodkkkkkrr78 <worldcup78trade@gmail.com>
2024-08-15 14:10:20 +0330

    Update urls.py

--- telegram_bot_backend/urls.py
+++ telegram_bot_backend/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from your_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('webhooks/', include('tradingview.urls'))
+    path('', views.home, name='home'),
+    # ... your other URL patterns ...
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
